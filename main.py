ecf66b705bc4822d88f46a71ed8c3063adb2cf0b
Mickey <mickeyveldhuizen@gmail.com>
2024-09-30 12:20:54 +0200

    map

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
-import pygame.examples.aliens
-pygame.examples.aliens.main()
+width = int(input("width"))
+width_memory = width
+heigth = int(input("heigth"))
+line = ""
+number = "o"
+
+while heigth > 0:
+    while width > 0:
+        line += str(number) + " "
+        number = "o"
+        width -= 1
+    width = width_memory
+    print(line)
+    line = ""
+    heigth -= 1
